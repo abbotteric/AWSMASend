4f922fca4e022778ba506158fa2b9a1e08c8d389
Eric Abbott <eabbott@skyslope.com>
2015-08-18 14:20:06 -0700

    initial commit

---
+++ AWSMASend/AWSMASend/AWSMASend.h
@@ -0,0 +1,13 @@
+//
+//  AWSMASend.h
+//  AWSMASend
+//
+//  Created by Eric Abbott on 8/18/15.
+//  Copyright (c) 2015 SkySlope. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWSMASend : NSObject
+
+@end
---
+++ AWSMASend/AWSMASend/AWSMASend.m
@@ -0,0 +1,13 @@
+//
+//  AWSMASend.m
+//  AWSMASend
+//
+//  Created by Eric Abbott on 8/18/15.
+//  Copyright (c) 2015 SkySlope. All rights reserved.
+//
+
+#import "AWSMASend.h"
+
+@implementation AWSMASend
+
+@end
